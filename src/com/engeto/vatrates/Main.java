29d3ddb34485a29feb6e93813b24d1740d10ce7e
alceresna <alceresna@seznam.cz>
2022-08-27 23:24:49 +0200

    Correct prints

--- src/com/engeto/vatrates/Main.java
+++ src/com/engeto/vatrates/Main.java
@@ -15,12 +15,12 @@ public class Main {
 
         System.out.println("Celkový seznam států:\n"+list.printWithFullRate());
 
-        System.out.println("Seznam států se základní sazbou DPH vyšší než "+ LIMIT +" % a bez speciální sazby daně:\n"
+        System.out.println("Seznam států se základní sazbou VAT vyšší než "+ LIMIT +" % a bez speciální sazby daně:\n"
                 +list.printOverLimitWithFullRate(LIMIT));
 
         list.sortByFullRateValue(list.getListOfStatesOverLimit(LIMIT));
 
-        System.out.println("Seznam států se základní sazbou DPH vyšší než "+ LIMIT +" % a bez speciální sazby daně:\n"
+        System.out.println("Seznam států se základní sazbou VAT vyšší než "+ LIMIT +" % a bez speciální sazby daně:\n"
                 +list.printOverLimitWithFullRateAndReducedRate(LIMIT));
 
         System.out.println("====================");
